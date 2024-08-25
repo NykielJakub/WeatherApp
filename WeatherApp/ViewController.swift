74e3506b2d0622eb3957721488ba71872dd5e091
Jakub Nykiel <jakub.nykiel@theheart.dev>
2024-08-26 00:02:40 +0200

    Init project

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+// Copyright © 2024 Nykiel Jakub.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
