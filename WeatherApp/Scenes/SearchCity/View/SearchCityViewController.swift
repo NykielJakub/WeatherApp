adee1f880a15b6f32867edc0c77e6f8dc77c07ac
Jakub Nykiel <jakub.nykiel@theheart.dev>
2024-08-28 23:04:13 +0200

    Search with regex

--- WeatherApp/Scenes/SearchCity/View/SearchCityViewController.swift
+++ WeatherApp/Scenes/SearchCity/View/SearchCityViewController.swift
@@ -6,7 +6,55 @@ import UIKit
 
 final class SearchCityViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    private let contentView = ContentView()
+    private let searchController = UISearchController(searchResultsController: nil)
+
+    // MARK: - Lifecycle
+    
+    override func loadView() {
+        view = contentView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupSearch()
+        setupNavigation()
+    }
+    
+    // MARK: - Private
+    
+    private func setupSearch() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "search_placeholder".localized
+        searchController.searchBar.delegate = self
+    }
+    
+    private func setupNavigation() {
+        title = "weather".localized
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchController
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+
+extension SearchCityViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        print(text)
+    }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension SearchCityViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        return text.matches(regex: .searchRegex)
     }
 }
